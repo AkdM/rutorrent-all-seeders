97063daa6c40b9b77603d48a50815dc3d8ebe30c
Micdu70 <Micdu70@users.noreply.github.com>
2019-03-10 10:36:39 +0100

    Update init.js
    Do simple and it works... ^^

--- init.js
+++ init.js
@@ -2,20 +2,12 @@ if (plugin.canChangeColumns()) {
   plugin.config = theWebUI.config;
   theWebUI.config = function(data) {
     // Add the column to the torrent list
-    theWebUI.tables.trt.columns.push({text: 'All Seeders', width: '100px', id: 'all_seeders', type: TYPE_NUMBER});
+    theWebUI.tables.trt.columns.push({text: 'All Seeders', width: '100px', id: 'seeds_all', type: TYPE_NUMBER});
     
-    // Add the data to the torrent
-    theWebUI.webui_addTorrents = theWebUI.addTorrents;
-    theWebUI.addTorrents = function(data) {
-      $.each(data.torrents, function(hash, torrent) {
-        torrent.all_seeders = parseInt(torrent.seeds_all)
-      });
-      theWebUI.webui_addTorrents(data);
-    }
     plugin.config.call(this, data);
   }
 }
 
 plugin.onRemove = function() {
-  theWebUI.getTable("trt").removeColumnById("all_seeders");
+  theWebUI.getTable("trt").removeColumnById("seeds_all");
 }
